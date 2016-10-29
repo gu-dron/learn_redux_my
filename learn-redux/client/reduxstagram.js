6cc1d500288b3512656be9ed4755f372a40a83b7
gu-dron <gu-dron@yandex.ru>
2016-10-30 02:24:11 +0300

    import react for project

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+//import css
+import css from './styles/style.styl';
+
+render (<h1>hello</h1>, document.getElementById('root'));
+
+
